a80aa6eff34f2c951e1cf1dfb036f8fd92a2006d
Dan Oneata <dan.oneata@gmail.com>
2024-12-10 10:23:11 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="probing_norms",
+    version="0.0.1",
+    url="https://github.com/danoneata/probing-norms",
+    author="Dan Oneață",
+    author_email="dan.oneata@gmail.com",
+    description="Understanding deep learning models by probing feature norms",
+    packages=find_packages(),
+    install_requires=["black", "click", "streamlit", "ruff"],
+)
